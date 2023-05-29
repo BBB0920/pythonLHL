798fee068511782b0085cb31d65c8a03af0d5795
Lyon <lyon.b.lee@gmail.com>
2023-05-29 16:44:29 -0400

    Getting Started - Variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+# name is the variable. It contains a string.
+# name = "Thomas"
+name = input("Please type in your name and press Enter -> ")
+print("Hello, " + name + ", it's very nice to meet you.")
+
+# taxRate, subtotal, tax, and total are all variables that contain numbers.
+# taxRate = 0.14
+# subtotal = 20
+# tax = subtotal * taxRate
+# total = subtotal + tax
+# print("Your total bill is:")
+# print(total)
+
+# # travelDestinations is a varaible containing a list.
+# # city is a variable - its value changes a few times. 
+# travelDestinations = ["Almaty", "Moscow", "Buenos Aires", "Manila"]
+# for city in travelDestinations:
+#   print(city + " seems like a cool place to go.")
