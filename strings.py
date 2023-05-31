49a1365c806295dd65c811933115be84845da98a
Lyon <lyon.b.lee@gmail.com>
2023-05-31 02:30:50 -0400

    Introductions to Strings

---
+++ strings.py
@@ -0,0 +1,21 @@
+# Introduction to Strings
+
+# Backslash denotes "the character immediately after me should be interpreted differently than usual"
+
+# print("She asked, \"Can't we just leave?\" If only it were that simple.")
+# print('She asked, "Can\'t we just leave?" If only it were that simple.')
+
+# Escape characters
+# https://python-reference.readthedocs.io/en/latest/docs/str/escapes.html
+
+# \n is new line
+print('Testing escape characters: \\n Tested "\n"')
+
+# \a produces a beep sound
+print('Testing escape characters: \\a Tested "\a"')
+
+# \t is tab
+print('Testing escape characters: \\t Tested "\t"')
+
+# \v is a vertical tab - not used that often, don't worry about it
+print('Testing escape characters: \\v Tested "\v"')
