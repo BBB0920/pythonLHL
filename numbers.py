de590f7fb6366de2f6538c54ff65baef9bec8945
Lyon <lyon.b.lee@gmail.com>
2023-06-03 00:41:29 -0400

    Exercise: Tax Calculator

--- numbers.py
+++ numbers.py
@@ -9,7 +9,16 @@
 # a // b - This is Integer Division, where the result will be rounded down so there are no decimal places
 # a ** b - This is Exponentiation (a to the power of b)
 
-a = 2
-b = 3
-c = 4
-print(a + b * c ** a)
+# a = 2
+# b = 3
+# c = 4
+# print(a + b * c ** a)
+
+# Exercise: Tax Calculator
+
+subtotal = input("What's your subtotal? ")
+taxRate = 0.25
+tax = subtotal * taxRate
+total = subtotal + tax
+print("Your tax is " + str(tax))
+print("Your total is: " + str(total))
