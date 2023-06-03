a88c16607da517c0dc07d35b3f3a32eeeaa1ace1
Lyon <lyon.b.lee@gmail.com>
2023-06-03 00:34:51 -0400

    Math in Python

--- numbers.py
+++ numbers.py
@@ -1,7 +1,15 @@
 # Introduction to Numbers
 # int always rounds down to nearest whole number
 
-print(float(2))
-print(int(2.0))
-print(int(2.9))
+# print(float(2))
+# print(int(2.0))
+# print(int(2.9))
 
+# Math in Python
+# a // b - This is Integer Division, where the result will be rounded down so there are no decimal places
+# a ** b - This is Exponentiation (a to the power of b)
+
+a = 2
+b = 3
+c = 4
+print(a + b * c ** a)
