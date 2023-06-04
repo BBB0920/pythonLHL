5c3d27a28e23cf0eda7f71d0d9836171ded95368
Lyon <lyon.b.lee@gmail.com>
2023-06-04 02:50:44 -0400

    Exercise: Reading Lists

---
+++ lists.py
@@ -0,0 +1,12 @@
+largestLandAnimals = [
+  "African Elephant",
+  "Asian Elephant",
+  "White Rhinoceros",
+  "Hippopotamus",
+  "Gaur",
+  "Giraffe"
+]
+
+print(largestLandAnimals[5])
+print(largestLandAnimals[-1])
+print(largestLandAnimals[0])
