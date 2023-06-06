d035fe42f07027129625bac374410ec604d5bd28
Lyon <lyon.b.lee@gmail.com>
2023-06-05 23:47:41 -0400

    Changing the Contents of a List

--- lists.py
+++ lists.py
@@ -10,3 +10,10 @@ largestLandAnimals = [
 print(largestLandAnimals[5])
 print(largestLandAnimals[-1])
 print(largestLandAnimals[0])
+
+# Changing the Contents of a list
+
+# append - add to the end of the array
+# insert - add at index (insert(index, "task"))
+# del - delete at index (del array[index])
+# + - combines two arrays into one
