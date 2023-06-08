18d67ec53615b9fa2a61c7215ea1541a59f82a98
Lyon <lyon.b.lee@gmail.com>
2023-06-08 00:04:00 -0400

    Making New Data

--- loops.py
+++ loops.py
@@ -1,10 +1,40 @@
-clothing = [
-  "Shirt",
-  "Pants",
-  "Socks"
-]
+# Introduction to looping
+# Indentation indicates that the lines belong to non-indented line
 
-for item in clothing:
-  foldedItem = "Folded " + item
-  print(foldedItem)
-  print("This line is not part of the `for` loop")
+# clothing = [
+#   "Shirt",
+#   "Pants",
+#   "Socks"
+# ]
+
+# for item in clothing:
+#   foldedItem = "Folded " + item
+#   print(foldedItem)
+#   print("This line is not part of the `for` loop")
+
+# Making New Data
+
+# instructionSteps = [
+#   "turn left",
+#   "go straight for 2 blocks",
+#   "turn right",
+#   "keep going until you see the dog statue",
+#   "turn right",
+#   "turn right again",
+#   "park right on the sidewalk"
+# ]
+
+# instructions = "First, "
+
+# for nextInstruction in instructionSteps:
+#   instructions = instructions + nextInstruction + ", then "
+
+# print(instructions + "you're there!")
+
+# instructionStepsButScreemed = []
+
+# for nextInstruction in instructionSteps:
+#   upperInstruction = nextInstruction.upper()
+#   instructionStepsButScreemed.append(upperInstruction)
+
+# print(instructionStepsButScreemed)
