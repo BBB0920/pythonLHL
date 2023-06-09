ed1948b2aadf4cb46ef409f1ef1777d102caa469
Lyon <lyon.b.lee@gmail.com>
2023-06-08 22:44:45 -0400

    Solution: Actors

--- loops.py
+++ loops.py
@@ -65,19 +65,51 @@ actors = [
   "Ron Glass"
 ]
 
-roles = [
-  "Captain Malcolm Reynolds",
-  "Zoe Washburn",
-  "Hoban Washburn",
-  "Inara Serra",
-  "Jayne Cobb",
-  "Kaylee Frye",
-  "Dr. Simon Tam",
-  "River Tam",
-  "Derrial Book"
-]
+# roles = [
+#   "Captain Malcolm Reynolds",
+#   "Zoe Washburn",
+#   "Hoban Washburn",
+#   "Inara Serra",
+#   "Jayne Cobb",
+#   "Kaylee Frye",
+#   "Dr. Simon Tam",
+#   "River Tam",
+#   "Derrial Book"
+# ]
 
-index = 8
+# for index in range(0, len(actors)):
+#   print(actors[index] + " as " + roles[index])
 
-for indice in range(0, index):
-  print(actors[indice] + " as " + roles[indice])
+# # Solution: Actors
+# # Enumerate - has access to index and the item 
+# for index, actor in enumerate(actors):
+#   print(actor + " as " + roles[index])
+
+# Refactor to be a List of lists
+
+# actorRoles = [
+#   ["Nathan Fillion", "Captain Malcolm Reynolds"],
+#   ["Gina Torres", "Zoe Washburn"],
+#   ["Alan Tudyk", "Hoban Washburn"],
+#   ["Morena Baccarin", "Inara Serra"],
+#   ["Adam Baldwin", "Jayne Cobb"],
+#   ["Jewel Staite", "Kaylee Frye"],
+#   ["Sean Maher", "Dr. Simon Tam"],
+#   ["Summer Glau", "River Tam"],
+#   ["Ron Glass", "Derrial Book"]
+# ]
+
+print('Featuring:\n=-=-=-=-=-=-=-=')
+# for actorRole in actorRoles:
+#   actor = actorRole[0]
+#   role = actorRole[1]
+#   print(actor + " as " + role)
+
+# below is refactored version of the above
+# for actor, role in actorRoles:
+#   print(actor + " as " + role)
+
+enumerableActors = enumerate(actors)
+print(enumerableActors)
+enumerableActorsList = list(enumerableActors)
+print(enumerableActorsList)
